4618665df6296a8fb0526c2e259b806ce49f4e61
Kenneth Heutmaker <ken@keepkey.com>
2015-05-15 17:33:18 -0700

    Bug fixes: Button request controller shows no text. Error loading a non-existent CSS file.

--- src/app/buttonRequest/buttonRequestController.js
+++ src/app/buttonRequest/buttonRequestController.js
@@ -2,7 +2,14 @@ angular.module('kkWallet')
     .controller('ButtonRequestController', ['$scope', '$routeParams', 'DeviceBridgeService', 'ProxyInfoService',
         function ButtonRequestController($scope, $routeParams, deviceBridgeService, proxyInfoService) {
             $scope.buttonRequestType = $routeParams.code;
-            $scope.firmwareFingerprint = proxyInfoService.info.imageHashCode.match(/.{1,40}/g);
+            $scope.proxyInfo = proxyInfoService.info;
+            $scope.$watch('proxyInfo.imageHashCodeTrezor', function(newVal) {
+                if (newVal) {
+                    $scope.firmwareFingerprint = newVal.match(/.{1,40}/g);
+                } else {
+                    $scope.firmwareFingerprint = [];
+                }
+            });
             $scope.onCancel = function() {
                 deviceBridgeService.cancel();
             };
