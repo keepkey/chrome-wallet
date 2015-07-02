a58f513821a85ea97c760b0d724747a3f501f20e
Kenneth Heutmaker <ken@keepkey.com>
2015-07-02 14:12:40 -0700

    - Fixed a bug when detecting wallets that need a public key. - removed indexeddb functionality.

--- src/app/services/WalletNodeService.js
+++ src/app/services/WalletNodeService.js
@@ -1,28 +1,15 @@
-/* global _, bitcore */
+/* global _ */
 
 angular.module('kkWallet')
     .factory('WalletNodeService', ['$rootScope', 'DeviceBridgeService',
         function DeviceFeatureService($rootScope, deviceBridgeService) {
             var nodes = [];
 
-            function calculatePublicKeys() {
-                var HDPrivateKey = bitcore.HDPrivateKey;
-                var Address = bitcore.Address;
-                var Networks = bitcore.Networks;
-
-                var hdPrivateKey = new HDPrivateKey();
-                var hdPublicKey = hdPrivateKey.hdPublicKey;
-
-                var addresses = new Address(hdPublicKey.derive(20).publicKey, Networks.livenet);
-
-                console.log('Addresses:', addresses);
-            }
-
             function updateWalletNodes(newNodes) {
                 angular.extend(nodes, newNodes);
                 // Request public key for nodes where it is missing
                 var nodesMissingPublicKey = _.filter(nodes, function (it) {
-                    return !nodes.hdNode;
+                    return !it.xpub;
                 });
                 _.each(nodesMissingPublicKey, function (it) {
                         deviceBridgeService.getPublicKey({
