c6175f53d43b6497083e66354bc808afa98fcc9c
Kenneth Heutmaker <ken@keepkey.com>
2015-07-15 17:59:06 -0700

    -Cleaned up the send transaction screen. -build transaction request sends the wallet that the transaction should be sent from.

--- src/app/buildTransaction/BuildTransactionController.js
+++ src/app/buildTransaction/BuildTransactionController.js
@@ -1,10 +1,10 @@
 angular.module('kkWallet')
-    .controller('BuildTransactionController', ['$scope', 'DeviceBridgeService', 'NavigationService', 'WalletNodeService', 'TransactionService',
-        function BuildTransactionController($scope, deviceBridgeService, navigationService, walletNodeService, transactionService) {
+    .controller('BuildTransactionController', ['$scope', '$routeParams', 'DeviceBridgeService', 'NavigationService', 'WalletNodeService', 'TransactionService',
+        function BuildTransactionController($scope, $routeParams, deviceBridgeService, navigationService, walletNodeService, transactionService) {
             $scope.transactionInProgress = false;
             $scope.userInput = {
-                sourceIndex: 0,
-                sourceName: walletNodeService.wallets[0].name,
+                sourceIndex: $routeParams.wallet,
+                sourceName: walletNodeService.wallets[$routeParams.wallet].name,
                 address: walletNodeService.firstUnusedAddress(walletNodeService.wallets[1].addresses[0]), //TODO This is an odd default.
                 amount: 0.001
             };
