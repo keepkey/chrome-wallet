202b5edcbf894ba3a50928df54dca4390c59d266
Darin Stanchfield <darin@Darins-MacBook-Pro.local>
2015-07-24 18:43:43 -0700

    removed flicker of disconnect screen

--- src/app/connect/ConnectController.js
+++ src/app/connect/ConnectController.js
@@ -1,10 +1,13 @@
 angular.module('kkWallet')
-    .controller('ConnectController', ['DeviceBridgeService',
-        function (deviceBridgeService) {
+    .controller('ConnectController', ['$scope', 'DeviceBridgeService',
+        function ($scope, deviceBridgeService) {
+            $scope.noDevice = false;
             deviceBridgeService.isDeviceReady().then(function (response) {
                 console.log('device ready:', response.result);
                 if (response.result) {
                     deviceBridgeService.initialize();
+                } else {
+                    $scope.noDevice = true;
                 }
             });
         }
