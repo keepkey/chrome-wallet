be566478cbf7ec91b92333481b5bf3d2673b68c0
Kenneth Heutmaker <ken@keepkey.com>
2015-07-28 05:15:28 -0400

    Change to the way wallets are indexed.

--- src/app/buildTransaction/BuildTransactionController.js
+++ src/app/buildTransaction/BuildTransactionController.js
@@ -2,9 +2,10 @@ angular.module('kkWallet')
     .controller('BuildTransactionController', ['$scope', '$routeParams', 'DeviceBridgeService', 'NavigationService', 'WalletNodeService', 'TransactionService',
         function BuildTransactionController($scope, $routeParams, deviceBridgeService, navigationService, walletNodeService, transactionService) {
             $scope.transactionInProgress = false;
+            $scope.wallet = _.find(walletNodeService.wallets, {id: parseInt($routeParams.wallet, 10)});
             $scope.userInput = {
                 sourceIndex: $routeParams.wallet,
-                sourceName: walletNodeService.wallets[$routeParams.wallet].name,
+                sourceName: $scope.wallet.name,
                 address: walletNodeService.firstUnusedAddress(walletNodeService.wallets[1].addresses[0]), //TODO This is an odd default.
                 amount: 0.001
             };
