9784fb151f4ae37ab961e37e271df51d7346a79d
Kenneth Heutmaker <ken@keepkey.com>
2015-08-10 10:28:44 -0700

    - Fixed bug where send transaction screen fails. - Reload wallet node gratutiously. - Hide send and receive buttons when waiting for public keys from the device. - Check wallet's public key when the wallet are first loaded. - bump verion to 0.0.28.

--- src/app/receive/ReceiveController.js
+++ src/app/receive/ReceiveController.js
@@ -1,43 +1,45 @@
 angular.module('kkWallet')
-    .controller('ReceiveController', ['$rootScope', '$scope', '$routeParams', '$location', 'DeviceBridgeService', 'WalletNodeService', 'NavigationService',
-        function ReceiveController($rootScope, $scope, $routeParams, $location, deviceBridgeService, walletNodeService, navigationService) {
-            navigationService.setNextTransition('slideLeft');
-            var deviceReadyPromise = new Promise(function(resolve) {
-                $rootScope.$on('ButtonRequest', function(ev, message) {
-                    if (message.code === 'ButtonRequest_Address') {
-                        resolve(message.code);
-                    }
-                });
-            });
+  .controller('ReceiveController', ['$rootScope', '$scope', '$routeParams', '$location', 'DeviceBridgeService', 'WalletNodeService', 'NavigationService',
+    function ReceiveController($rootScope, $scope, $routeParams, $location, deviceBridgeService, walletNodeService, navigationService) {
+      walletNodeService.reload();
 
-            $scope.address = $routeParams.address;
-            $scope.walletId = parseInt($routeParams.walletId, 10);
-            $scope.getBitcoinLink = function (address) {
-                return [
-                    'bitcoin:',
-                    address,
-                    '?label=KeepKey%20Wallet',
-                    '&message=Secure%20Bitcoins%20with%20your%20KeepKey%20wallet'].join('');
-            };
+      navigationService.setNextTransition('slideLeft');
+      var deviceReadyPromise = new Promise(function (resolve) {
+        $rootScope.$on('ButtonRequest', function (ev, message) {
+          if (message.code === 'ButtonRequest_Address') {
+            resolve(message.code);
+          }
+        });
+      });
 
-            $scope.$on("$destroy", function() {
-                if (deviceReadyPromise && $location.path() !== '/pin/pin_matrix_request_type_current') {
-                    deviceReadyPromise.then(function() {
-                        deviceBridgeService.cancel();
-                    });
-                }
-            });
+      $scope.address = $routeParams.address;
+      $scope.walletId = parseInt($routeParams.walletId, 10);
+      $scope.getBitcoinLink = function (address) {
+        return [
+          'bitcoin:',
+          address,
+          '?label=KeepKey%20Wallet',
+          '&message=Secure%20Bitcoins%20with%20your%20KeepKey%20wallet'].join('');
+      };
 
-            function displayAddressOnDevice() {
-                getAddressPromise = deviceBridgeService.getAddress({
-                    messageType: 'GetAddress',
-                    addressN: walletNodeService.getAddressNode(
-                        $scope.walletId, $scope.address),
-                    coinName: "Bitcoin",
-                    showDisplay: true
-                });
-            }
-
-            displayAddressOnDevice();
+      $scope.$on("$destroy", function () {
+        if (deviceReadyPromise && $location.path() !== '/pin/pin_matrix_request_type_current') {
+          deviceReadyPromise.then(function () {
+            deviceBridgeService.cancel();
+          });
         }
-    ]);
+      });
+
+      function displayAddressOnDevice() {
+        getAddressPromise = deviceBridgeService.getAddress({
+          messageType: 'GetAddress',
+          addressN: walletNodeService.getAddressNode(
+            $scope.walletId, $scope.address),
+          coinName: "Bitcoin",
+          showDisplay: true
+        });
+      }
+
+      displayAddressOnDevice();
+    }
+  ]);
