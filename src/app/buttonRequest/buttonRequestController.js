7c03e4efedec2d25d23ec1492cc25e307d184425
Darin Stanchfield <darin@Darins-MacBook-Pro.local>
2015-09-06 05:20:32 -0700

    added device features to scope

--- src/app/buttonRequest/buttonRequestController.js
+++ src/app/buttonRequest/buttonRequestController.js
@@ -1,9 +1,10 @@
 angular.module('kkWallet')
-    .controller('ButtonRequestController', ['$scope', '$routeParams', 'DeviceBridgeService', 'ProxyInfoService', 'NavigationService',
-        function ButtonRequestController($scope, $routeParams, deviceBridgeService, proxyInfoService, navigationService) {
+    .controller('ButtonRequestController', ['$scope', '$routeParams', 'DeviceBridgeService', 'ProxyInfoService', 'NavigationService', 'DeviceFeatureService',
+        function ButtonRequestController($scope, $routeParams, deviceBridgeService, proxyInfoService, navigationService, deviceFeatureService) {
             navigationService.setNextTransition('slideLeft');
             $scope.buttonRequestType = $routeParams.code;
             $scope.proxyInfo = proxyInfoService.info;
+            $scope.device = deviceFeatureService.features;
             $scope.$watch('proxyInfo.imageHashCodeTrezor', function(newVal) {
                 if (newVal) {
                     $scope.firmwareFingerprint = newVal.match(/.{1,40}/g);
