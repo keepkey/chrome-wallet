701858f2b72600941147204ddcd59e477652d454
Darin Stanchfield <darin@keepkey.com>
2016-04-18 13:46:46 -0700

    added correct slide animation

--- src/app/popup/failure/FailureController.js
+++ src/app/popup/failure/FailureController.js
@@ -1,7 +1,9 @@
 angular.module('kkWallet')
-    .controller('FailureController', ['$scope', '$timeout', 'FailureMessageService', 'DeviceBridgeService',
-        function FailureController($scope, $timeout, failureMessageService, deviceBridgeService) {
+  .controller('FailureController', ['$scope', '$timeout', 'FailureMessageService', 'DeviceBridgeService', 'NavigationService',
+    function FailureController($scope, $timeout, failureMessageService, deviceBridgeService, navigationService) {
       $scope.failures = failureMessageService.get();
+
+      navigationService.setNextTransition('slideRight');
 
       $scope.ok = function () {
         deviceBridgeService.initialize();
