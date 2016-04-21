33b437b4eec371b6681d0b2a0ba7a8c26eac97b0
Darin Stanchfield <darin@keepkey.com>
2016-04-21 14:39:41 -0700

    fixed transaction signing transitions

--- src/app/popup/buttonRequest/signTxController.js
+++ src/app/popup/buttonRequest/signTxController.js
@@ -1,7 +1,13 @@
 angular.module('kkWallet')
     .controller('SignTxController', ['$scope', '$routeParams', 'TransactionService', 'NavigationService',
         function ButtonRequestController($scope, $routeParams, transactionService, navigationService) {
-            navigationService.setNextTransition('slideRight');
+            
+            if(navigationService.getCurrentRoute() === '/buttonRequest/button_request_sign_tx') {
+              navigationService.setNextTransition('slideRight');
+            } else {
+              navigationService.setNextTransition('slideLeft');
+            }
+            
             $scope.buttonRequestType = $routeParams.code;
             $scope.amount = transactionService.transactionInProgress.amount;
             $scope.destination = transactionService.transactionInProgress.address;
