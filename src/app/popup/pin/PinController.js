a67cd9290ba071f23655b647a40b4ae27aae0799
Darin Stanchfield <darin@keepkey.com>
2016-04-21 12:28:39 -0700

    fixed transitions

--- src/app/popup/pin/PinController.js
+++ src/app/popup/pin/PinController.js
@@ -1,12 +1,15 @@
 angular.module('kkWallet')
-  .controller('PinController', ['$scope', '$routeParams', 'NavigationService',
-    function PinController($scope, $routeParams, navigationService) {
+  .controller('PinController', ['$scope', '$routeParams', 'NavigationService', 'DeviceFeatureService',
+    function PinController($scope, $routeParams, navigationService, deviceFeatureService) {
       var previousRoute = navigationService.getPreviousRoute();
       $scope.showDeviceConfigurationButton = false;
 
       if (previousRoute === '/walletlist') {
         $scope.successRoute = previousRoute;
         $scope.showDeviceConfigurationButton = true;
+      } else if(navigationService.getCurrentRoute() === '/pin/pin_matrix_request_type_new_second' &&
+        deviceFeatureService.features.initialized) {
+        navigationService.setNextTransition('slideRight');
       } else {
         navigationService.setNextTransition('slideLeft');
       }
