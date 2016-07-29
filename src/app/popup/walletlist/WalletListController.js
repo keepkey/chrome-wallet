8b191eaa612a264e17c67cef0d0862a11369c68e
Kenneth Heutmaker <ken@keepkey.com>
2016-07-29 10:43:00 -0700

    Sort wallet list by cointype.

--- src/app/popup/walletlist/WalletListController.js
+++ src/app/popup/walletlist/WalletListController.js
@@ -24,11 +24,18 @@ angular.module('kkWallet')
         $scope.go('/wallet/' + wallet.id, 'slideLeft');
       };
 
+      $scope.coinTypes = [];
+
+      $scope.walletsByCoinType = function(coinType) {
+        return _.filter($scope.wallets, {coinType: coinType});
+      };
+
       $scope.$watch("wallets.length", function() {
         $scope.loaded = !!$scope.wallets.length;
         if ($scope.wallets.length == 1) {
           $scope.go('/wallet/' + $scope.wallets[0].id);
         }
+        $scope.coinTypes = _.uniq(_.map($scope.wallets, 'coinType'));
       });
 
     }
