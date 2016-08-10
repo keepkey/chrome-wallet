c494bc66984ad6f5a505fad097cffa7cf46e1837
Kenneth Heutmaker <ken@keepkey.com>
2016-08-10 16:30:44 -0700

    Remove attempts to cancel an in progress transaction because it isn't well supported.

--- src/app/popup/buttonRequest/sign-exchange-controller.js
+++ src/app/popup/buttonRequest/sign-exchange-controller.js
@@ -4,7 +4,6 @@ angular.module('kkWallet')
     '$scope',
     function ($scope) {
       $scope.cancelExchangeRequest = function() {
-        console.error('TODO: Implement cancel exchange request in the ConfirmExchangeController');
         $scope.cancelDeviceOperation();
         $scope.goBack();
       }
--- src/app/popup/confirm-exchange/confirm-exchange-controller.js
+++ src/app/popup/confirm-exchange/confirm-exchange-controller.js
@@ -9,19 +9,20 @@ angular.module('kkWallet')
       $scope.withdrawalAmount = exchangeService.get('withdrawal.amount');
       $scope.withdrawalCurrency = exchangeService.get('withdrawal.coinType');
       $scope.withdrawalAccount = exchangeService.get('withdrawal.accountId');
-      $scope.withdrawalAccountName = walletNodeService.getWalletById($scope.withdrawalAccount).name;
-      $scope.withdrawalCurrencyName = walletNodeService.getWalletById($scope.withdrawalAccount).coinType;
+      $scope.withdrawalAccountName =
+        walletNodeService.getWalletById($scope.withdrawalAccount).name;
+      $scope.withdrawalCurrencyName =
+        walletNodeService.getWalletById($scope.withdrawalAccount).coinType;
       $scope.exchangeRate = exchangeService.get('rate');
       $scope.minerFee = exchangeService.get('minerFee');
       $scope.transactionId = exchangeService.get('transactionId');
 
       $scope.nextAction = function() {
         deviceBridgeService.confirmCurrencyExchange($scope.transactionId);
-      }
+      };
 
       $scope.cancelExchangeRequest = function() {
-        console.error('TODO: Implement cancel exchange request in the ConfirmExchangeController');
         $scope.goBack();
-      }
+      };
     }
   ]);
