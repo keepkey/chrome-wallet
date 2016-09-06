3113c58e54878dbf1a2b551ca92d3331bdd2f1a6
Kenneth Heutmaker <ken@keepkey.com>
2016-09-06 14:52:37 -0700

    FIx dust threshhold on the send screen for multicurrency

--- src/app/common/services/currency-lookup-service.js
+++ src/app/common/services/currency-lookup-service.js
@@ -5,19 +5,22 @@ angular.module('kkCommon')
         name: 'Bitcoin',
         currencySymbol: 'BTC',
         coinTypeCode: "0'",
-        addressRegExp: /^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/
+        addressRegExp: /^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/,
+        dust: 546
       },
       Litecoin: {
         name: 'Litecoin',
         currencySymbol: 'LTC',
         coinTypeCode: "2'",
-        addressRegExp: /^L[a-km-zA-HJ-NP-Z1-9]{26,33}$/
+        addressRegExp: /^L[a-km-zA-HJ-NP-Z1-9]{26,33}$/,
+        dust: 100000
       },
       Dogecoin: {
         name: 'Dogecoin',
         currencySymbol: 'DOGE',
         coinTypeCode: "3'",
-        addressRegExp: /^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$/
+        addressRegExp: /^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$/,
+        dust: 100000000
       }
     };
 
@@ -33,6 +36,9 @@ angular.module('kkCommon')
       },
       getCurrencyTypes: function () {
         return _.keys(coinType);
+      },
+      getDust: function getDust(currencyName) {
+        return _.get(coinType, [currencyName, 'dust'].join('.'));
       }
     }
   });
