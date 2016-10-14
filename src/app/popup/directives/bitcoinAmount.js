792e7ca304ee44c509503d6dfd5b0cfd67a692e0
Kenneth Heutmaker <ken@keepkey.com>
2016-10-14 12:36:51 -0700

    Provide proper rounding for ethereum amounts

--- src/app/popup/directives/bitcoinAmount.js
+++ src/app/popup/directives/bitcoinAmount.js
@@ -9,19 +9,16 @@ angular.module('kkWallet')
       },
       controller: ['$scope', 'CurrencyLookupService',
         function($scope, currencyLookupService) {
-          function toBitcoins(satoshis) {
-            return satoshis / 100000000;
-          }
-
           $scope.currencySymbol =
             currencyLookupService.getCurrencySymbol($scope.currency);
 
-          $scope.formattedAmount = toBitcoins(parseFloat($scope.amount));
+          $scope.formattedAmount =
+            currencyLookupService.formatAmount($scope.currency, $scope.amount);
 
           $scope.$watch('amount', function() {
             var amount = parseFloat($scope.amount);
-            $scope.formattedAmount =
-              (amount > 0) ? toBitcoins(amount) : '0.0';
+            $scope.formattedAmount = (amount > 0) ?
+              currencyLookupService.formatAmount($scope.currency, $scope.amount) : '0.0';
           });
         }
       ],
