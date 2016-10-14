81074f078b41cb44d54294badab8d61e3e296987
Kenneth Heutmaker <ken@keepkey.com>
2016-10-14 12:36:51 -0700

    Provide proper rounding for ethereum amounts

--- src/app/common/services/currency-lookup-service.js
+++ src/app/common/services/currency-lookup-service.js
@@ -6,28 +6,44 @@ angular.module('kkCommon')
         currencySymbol: 'BTC',
         coinTypeCode: "0'",
         addressRegExp: /^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/,
-        dust: 546
+        dust: 546,
+        decimals: 8,
+        displayAmountConstructor: BigNumber.another({
+          DECIMAL_PLACES: 8
+        })
       },
       Litecoin: {
         name: 'Litecoin',
         currencySymbol: 'LTC',
         coinTypeCode: "2'",
         addressRegExp: /^L[a-km-zA-HJ-NP-Z1-9]{26,33}$/,
-        dust: 100000
+        dust: 100000,
+        decimals: 8,
+        displayAmountConstructor: BigNumber.another({
+          DECIMAL_PLACES: 8
+        })
       },
       Dogecoin: {
         name: 'Dogecoin',
         currencySymbol: 'DOGE',
         coinTypeCode: "3'",
         addressRegExp: /^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$/,
-        dust: 100000000
+        dust: 100000000,
+        decimals: 8,
+        displayAmountConstructor: BigNumber.another({
+          DECIMAL_PLACES: 8
+        })
       },
       Ethereum: {
         name: 'Ethereum',
         currencySymbol: 'ETH',
         coinTypeCode: "60'",
         addressRegExp: /^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$/,
-        dust: "420000000000000"
+        dust: "420000000000000",
+        decimals: 18,
+        displayAmountConstructor: BigNumber.another({
+          DECIMAL_PLACES: 18
+        })
       }
     };
 
@@ -46,6 +62,17 @@ angular.module('kkCommon')
       },
       getDust: function getDust(currencyName) {
         return _.get(coinType, [currencyName, 'dust'].join('.'));
+      },
+      formatAmount: function (currencyName, amount) {
+        var currencySettings = _.get(coinType, currencyName);
+        return new currencySettings.displayAmountConstructor(amount)
+          .shift(-currencySettings.decimals);
+      },
+      unformatAmount: function(currencyName, amount) {
+        var currencySettings = _.get(coinType, currencyName);
+        return new BigNumber(amount)
+          .shift(currencySettings.decimals)
+          .round();
       }
     }
   });
