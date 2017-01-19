38213d1e02f5eb1c15fa813c967d839593ac0365
Kenneth Heutmaker <bgok@users.noreply.github.com>
2017-01-19 09:58:58 -0800

    Convert to launcher for chrome application
    * Reduce the functionality of the extension to launch the ui that is part of the proxy instead of showing it's own ui.
    * Rename the extension to KeepKey Launcher
    * Change the package name to reflect the new extension name.

--- src/app/popup/success/SuccessController.js
+++
@@ -1,7 +0,0 @@
-angular.module('kkWallet')
-  .controller('SuccessController', ['$scope', '$routeParams', 'NavigationService', 'WalletNodeService',
-    function SuccessController($scope, $routeParams, navigationService, walletNodeService) {
-      $scope.message = decodeURIComponent($routeParams.message);
-      navigationService.setNextTransition('slideLeft');
-    }
-  ]);
